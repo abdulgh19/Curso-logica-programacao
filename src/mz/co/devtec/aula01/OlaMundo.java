9cc32ed4909b4bab500f09be39104196eff64ba4
Momade Abdul <momademha@gmail.com>
2020-12-12 10:09:06 +0200

    Projecto java criado e a classe Ola Mundo

---
+++ src/mz/co/devtec/aula01/OlaMundo.java
@@ -0,0 +1,13 @@
+package mz.co.devtec.aula01;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Sejam bem vindos ao curso de Programacao!");
+	}
+
+}
+
+
+
+

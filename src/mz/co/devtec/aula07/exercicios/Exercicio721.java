399a743d9f42c100893a765dd86f2b790ff33a33
Momade Abdul <momademha@gmail.com>
2020-12-20 21:19:59 +0200

    Exercicio721

---
+++ src/mz/co/devtec/aula07/exercicios/Exercicio721.java
@@ -0,0 +1,25 @@
+package mz.co.devtec.aula07.exercicios;
+
+public class Exercicio721 {
+
+	public static void main(String[] args) {
+
+				
+		String [] verdurasELegumes = new String [7];
+
+//Primeira forma aprendida
+		verdurasELegumes[0] = "papaia";
+		verdurasELegumes[0] = "couve";
+		verdurasELegumes[0] = "alface";
+		verdurasELegumes[0] = "laranja";
+		verdurasELegumes[0] = "morango";
+		verdurasELegumes[0] = "uva";
+		verdurasELegumes[0] = "floha de abobora";
+		
+
+//Segunda forma aprendida
+		String [] verdurasELeguminosas = {"papaia", "couve", "alface", "laranja", "morango", "uva", "folha de abóbora"};
+		
+	}
+
+}
